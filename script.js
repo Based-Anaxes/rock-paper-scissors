08332dd14126a5471d3181553daad00acaf176ef
Based-Anaxes <anaxes808@gmail.com>
2024-03-07 15:59:46 -0500

    Change getComputerSelection to use switch case
    This function previously used an else if statement to calculate the
    selection for the computer where it randomized a number between 1 and 3
    and then converted those numbers to variables based on the output. It
    now still does this, but the conversion is made using a switch case to
    make the code cleaner.

--- script.js
+++ script.js
@@ -1,97 +1,18 @@
-const rock = "Rock";
-const paper = "Paper";
-const scissors = "Scissors";
+const ROCK = "Rock";
+const PAPER = "Paper";
+const SCISSORS = "Scissors";
 
-let playerScore = 0;
-let computerScore = 0;
-
-function getComputerChoice (rock, paper, scissors) {
-    let getComputerChoice = Math.floor(Math.random() * 3) + 1;
-    if (getComputerChoice === 1) {
-        getComputerChoice = rock
-    } else if (getComputerChoice == 2) {
-        getComputerChoice = paper
-    } else {
-        getComputerChoice = scissors
-    }
-    return getComputerChoice;
-}
-
-function getPlayerChoice (playerSelection){
-let getPlayerSelection= prompt("Rock, paper, scissors, shoot!");
-    switch (getPlayerSelection.toLowerCase()){
-        case "rock":
-            playerSelection = rock;
+function getComputerSelection() {
+    switch (computerSelection = Math.floor((Math.random() * 3) + 1)) {
+        case 1:
+            computerSelection = ROCK;
             break;
-        case "paper":
-            playerSelection = paper;
-            break;
-        case "scissors":
-            playerSelection = scissors;
-            break;
-        default:
-            alert("Invalid Input!")
-            break;
-    } 
-    return playerSelection;
+        case 2: 
+            computerSelection = PAPER;
+            break; 
+        case 3: 
+            computerSelection = SCISSORS;
+    }       
+        return computerSelection;
 }
-
-function computerSelection (){
-    return getComputerChoice(rock, paper, scissors);
-}
-
-function playRound (playerSelection, computerSelection) {
-    playerSelection = (getPlayerChoice(playerSelection));
-    console.log (playerSelection);
-    computerSelection = (getComputerChoice(rock, paper, scissors));
-    console.log (computerSelection);
-    
-    if (playerSelection == rock && computerSelection == scissors) {
-        console.log("VICTORY! Rock SMASHES Scissors!");
-        return ++playerScore;
-    } else if (playerSelection == scissors && computerSelection == rock) {
-        console.log("DEFEAT! Rock SMASHES Scissors!");
-        return ++computerScore;
-    } else if (playerSelection == paper && computerSelection == rock) {
-        console.log("VICTORY! Paper COVERS Rock!");
-        return ++playerScore;
-    } else if (playerSelection == rock && computerSelection == paper) {
-        console.log("DEFEAT! Paper COVERS Rock!");
-        return ++computerScore;
-    } else if (playerSelection == scissors && computerSelection == paper) {
-        console.log("VICTORY! Scissors CUT Paper!");
-        return ++playerScore;
-    } else if (playerSelection == paper && computerSelection == scissors) {
-        console.log("DEFEAT! Scissors CUT Paper!");
-        return ++computerScore;
-        }
-    else {
-        console.log("TIE!");
-        return score = 0;
-    }
-
-}
-
-function playGame() {
-    for(round = 1; round < 6; round++){
-    let round = (playRound(getPlayerChoice, computerSelection));
-    console.log(round);
-    }
-    if (playerScore > computerScore) {
-        result = "YOU WIN!";
-    } else if (playerScore < computerScore) {
-        result = "COMPUTER WINS!";
-    } else {
-        result = "IT'S A TIE!";
-    }
-
-    console.log("YOUR SCORE = ");
-    console.log(playerScore);
-    console.log("COMPUTER SCORE = ");
-    console.log(computerScore);
-    round ++;
-    console.log(result);
-
-}
-
-playGame();
+console.log(getComputerSelection());
