a511a03bf090fbc884fa8179cb6d9ae4cfc68fa0
Based-Anaxes <anaxes808@gmail.com>
2024-03-11 14:53:42 -0400

    Add event listener and handler to buttons
    This makes it so that whenever a player clicks a button, the playRound
    function is called with the correct player selection.

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-const CHOICES = ["Rock", "Paper", "Scissors"]; 
-const BUTTONS = document.querySelectorAll('button');
-const PLAYER_SELECTION = Array.from(BUTTONS);
+const choices = ["Rock", "Paper", "Scissors"]; 
+const buttons = document.querySelectorAll('button');
+const game = document.querySelector('.game');
+const roundResult = document.createElement('div')
 
-function getPlayerSelection() {
+let round = 0;
+let playerScore = 0;
+let computerScore = 0; 
+let computerSelection = choices[Math.floor(choices.length * Math.random())];
 
+buttons.forEach((button) => {
+    button.addEventListener("click", (button) => {
+        if (button.target.matches('#rock')) {
+            playerChoice = choices[0];
+        } else if (button.target.matches('#paper')) {
+            playerChoice = choices[1];
+        } else {
+            playerChoice = choices[2];
+        }
+        playRound(playerChoice, computerSelection);
+    })
+})
+
+function playRound(playerChoice, computerSelection) {
+    if (playerChoice == choices[0] && computerSelection == choices[0]) {
+        game.appendChild(results);
+        results.textContent = ;
+        console.log("worked");
+        console.log("It's a tie!");
+    }
 }
-console.log(BUTTONS);
-console.log(PLAYER_SELECTION);
-function getComputerChoice() {
-    let computerChoice = CHOICES[Math.floor(CHOICES.length * Math.random())];
-    return computerChoice;
-}
-
-function playRound(...BUTTONS) {
-
-}
-console.log(getComputerChoice());
-console.log(getPlayerSelection());
+console.log(computerSelection);
